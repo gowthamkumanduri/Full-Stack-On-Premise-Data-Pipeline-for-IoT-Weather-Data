0a0e5ad65fb303ef0caa527cbdb24b3038f1b944
gowthamkumanduri <32899777+gowthamkumanduri@users.noreply.github.com>
2025-11-03 11:30:34 +0530

    Update batch_etl_dag.py

--- batch_etl_dag.py
+++ batch_etl_dag.py
@@ -32,28 +32,3 @@ run_etl = BashOperator(
     env=env_vars,
     dag=dag
 )
-
-root@ip-172-31-24-80:~/weather_pipeline_project# cat airflow/dags/weather_to_kafka_dag.py
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime
-
-default_args = {
-    'owner': 'gowtham',
-    'start_date': datetime(2025, 10, 27)
-}
-
-dag = DAG(
-    'weather_to_kafka',
-    default_args=default_args,
-    schedule_interval='* * * * *',
-    catchup=False,
-    max_active_runs=1
-)
-
-push_weather = BashOperator(
-    task_id='push_weather_to_kafka',
-    bash_command='python3 /root/weather_pipeline_project/kafka/weather_to_kafka.py',
-    dag=dag
-)
-
